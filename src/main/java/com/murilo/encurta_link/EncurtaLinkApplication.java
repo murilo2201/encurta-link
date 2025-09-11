9395f0aa68f307a9533dcbc58da8c8de082a4e32
MuriloF <murilofs.franca@gmail.com>
2025-09-11 16:41:21 -0300

    start project: encurta-link

---
+++ src/main/java/com/murilo/encurta_link/EncurtaLinkApplication.java
@@ -0,0 +1,13 @@
+package com.murilo.encurta_link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtaLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtaLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/murilo/encurta_link/EncurtaLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilo.encurta_link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtaLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
